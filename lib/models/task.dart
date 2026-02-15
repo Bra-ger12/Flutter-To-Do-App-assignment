9e5b8d4ac4c2cc74eeb50214ec690b62ca1c50ad
Samia <hamadsamia37@gmail.com>
2026-02-15 23:17:29 +0300

    modifying the model

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,21 +1,16 @@
+
 class Task {
   String title;
-  String? description; 
-  DateTime? dueDate;   
   bool isDone;
 
   Task({
     required this.title,
-    this.description,
-    this.dueDate,
     this.isDone = false,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'title': title,
-      'description': description,
-      'dueDate': dueDate?.toIso8601String(),
       'isDone': isDone,
     };
   }
@@ -23,10 +18,6 @@ class Task {
   factory Task.fromMap(Map<String, dynamic> map) {
     return Task(
       title: map['title'],
-      description: map['description'],
-      dueDate: map['dueDate'] != null
-          ? DateTime.parse(map['dueDate'])
-          : null,
       isDone: map['isDone'],
     );
   }
